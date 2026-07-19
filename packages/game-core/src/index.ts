3ae768ec37aef3e916673991cdb8d4390baa469a
MrFr3di <ynagi-wm@mail.ru>
2026-07-20 01:40:23 +0500

    feat: export determinism kernel

--- packages/game-core/src/index.ts
+++ packages/game-core/src/index.ts
@@ -1,1 +1,11 @@
-export {};
+export {
+  canonicalizeAuthoritative,
+  type AuthoritativeJsonValue,
+} from "./determinism/authoritative-json";
+export {
+  fingerprint,
+  sha256Hex,
+  stableId,
+} from "./determinism/hash";
+export type { RandomSource } from "./determinism/random-source";
+export { Xoshiro256StarStar } from "./determinism/xoshiro256ss";
