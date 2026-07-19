d7f3f742eff29e97739ab1d5f71153d69523d6cc
MrFr3di <ynagi-wm@mail.ru>
2026-07-20 01:29:08 +0500

    feat: export determinism contracts

--- packages/game-schema/src/index.ts
+++ packages/game-schema/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export {
+  DETERMINISM_MANIFEST_V1,
+  parseSerializedXoshiro256State,
+} from "./determinism";
+export type {
+  DeterminismManifest,
+  Fingerprint,
+  SerializedXoshiro256State,
+  StableId,
+} from "./determinism";
