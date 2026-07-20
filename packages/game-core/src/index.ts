5d3c872d205feb4179430ddc6057dc314676d3ef
MrFr3di <ynagi-wm@mail.ru>
2026-07-20 07:20:22 +0500

    refactor: re-export authoritative JSON schema type

--- packages/game-core/src/index.ts
+++ packages/game-core/src/index.ts
@@ -1,7 +1,5 @@
-export {
-  canonicalizeAuthoritative,
-  type AuthoritativeJsonValue,
-} from "./determinism/authoritative-json";
+export { canonicalizeAuthoritative } from "./determinism/authoritative-json";
 export { fingerprint, sha256Hex, stableId } from "./determinism/hash";
 export type { RandomSource } from "./determinism/random-source";
 export { Xoshiro256StarStar } from "./determinism/xoshiro256ss";
+export type { AuthoritativeJsonValue } from "@runtime-human/game-schema";
