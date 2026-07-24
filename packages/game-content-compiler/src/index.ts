4ab6ab3223e24d0d1ff9b4775557d99a72a1afc1
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 00:21:38 +0500

    feat: export content source discovery

--- packages/game-content-compiler/src/index.ts
+++ packages/game-content-compiler/src/index.ts
@@ -1,4 +1,6 @@
 export { compileContentSources } from "./compile-content-sources";
 export type { ContentDiagnostic, ContentDiagnosticCode } from "./content-diagnostics";
 export { CONTENT_COMPILER_VERSION, CONTENT_SOURCE_SCHEMA_V1 } from "./content-source-schema";
+export { loadContentSourceFiles } from "./load-content-source-files";
+export type { LoadContentSourceFilesOptions } from "./load-content-source-files";
 export type { CompileContentResult, ContentSourceFile } from "./content-types";
