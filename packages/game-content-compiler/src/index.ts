37733b98b03f4a23f0d6ae177bdc3e7d5076c362
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 00:30:14 +0500

    feat: export compiled content publication

--- packages/game-content-compiler/src/index.ts
+++ packages/game-content-compiler/src/index.ts
@@ -3,4 +3,9 @@ export type { ContentDiagnostic, ContentDiagnosticCode } from "./content-diagnos
 export { CONTENT_COMPILER_VERSION, CONTENT_SOURCE_SCHEMA_V1 } from "./content-source-schema";
 export { loadContentSourceFiles } from "./load-content-source-files";
 export type { LoadContentSourceFilesOptions } from "./load-content-source-files";
+export { checkContentArtifacts, writeContentArtifacts } from "./write-content-artifacts";
+export type {
+  ContentArtifactsCheck,
+  ContentArtifactsOptions,
+} from "./write-content-artifacts";
 export type { CompileContentResult, ContentSourceFile } from "./content-types";
