4f0b10deaf26c7ac24d807613a368d19a35aefbd
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 00:54:27 +0500

    feat: export content build project API

--- packages/game-content-compiler/src/index.ts
+++ packages/game-content-compiler/src/index.ts
@@ -1,8 +1,17 @@
 export { compileContentSources } from "./compile-content-sources";
+export { parseContentBuildConfig } from "./content-build-config";
+export type { ContentBuildConfig } from "./content-build-config";
 export type { ContentDiagnostic, ContentDiagnosticCode } from "./content-diagnostics";
 export { CONTENT_COMPILER_VERSION, CONTENT_SOURCE_SCHEMA_V1 } from "./content-source-schema";
+export { formatContentDiagnostics } from "./format-content-diagnostics";
 export { loadContentSourceFiles } from "./load-content-source-files";
 export type { LoadContentSourceFilesOptions } from "./load-content-source-files";
+export { runContentBuild } from "./run-content-build";
+export type {
+  ContentBuildMode,
+  ContentBuildResult,
+  RunContentBuildOptions,
+} from "./run-content-build";
 export { checkContentArtifacts, writeContentArtifacts } from "./write-content-artifacts";
 export type { ContentArtifactsCheck, ContentArtifactsOptions } from "./write-content-artifacts";
 export type { CompileContentResult, ContentSourceFile } from "./content-types";
