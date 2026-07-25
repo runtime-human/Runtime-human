4ad193e3fda26534196d771e46aecc0273528fb0
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 20:37:09 +0500

    feat: export January plan parser and RNG budget

--- packages/game-core/src/january-1990/index.ts
+++ packages/game-core/src/january-1990/index.ts
@@ -33,7 +33,7 @@ export type { January1990ContentId } from "./january-content-ids";
 export { JANUARY_1990_DECISION_IDS } from "./january-decisions";
 export type { January1990DecisionId } from "./january-decisions";
 export { createJanuary1990MonthSteps } from "./january-month-steps";
-export { createJanuary1990MonthPlan } from "./january-month-plan";
+export { createJanuary1990MonthPlan, parseJanuary1990MonthPlan } from "./january-month-plan";
 export type { January1990MonthPlanV1 } from "./january-month-plan";
 export {
   createJanuary1990Result,
@@ -53,5 +53,8 @@ export type {
 } from "./january-provisional-state";
 export { JANUARY_1990_REASON_CODES } from "./january-reason-codes";
 export type { January1990ReasonCode } from "./january-reason-codes";
-export { JANUARY_1990_RNG_SCOPES } from "./january-rng-scopes";
+export {
+  JANUARY_1990_RNG_CALL_BUDGET,
+  JANUARY_1990_RNG_SCOPES,
+} from "./january-rng-scopes";
 export type { January1990RngScope } from "./january-rng-scopes";
