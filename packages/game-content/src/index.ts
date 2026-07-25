fe1aa0c7ba9c884d1063edeb8f1908173622df7a
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 16:59:08 +0500

    feat: export compiled content runtime APIs

--- packages/game-content/src/index.ts
+++ packages/game-content/src/index.ts
@@ -1,1 +1,5 @@
 export * from "./compiled-content";
+export * from "./compiled-content-runtime";
+export * from "./content-errors";
+export * from "./content-loader";
+export type { ContentRegistry } from "./content-registry";
