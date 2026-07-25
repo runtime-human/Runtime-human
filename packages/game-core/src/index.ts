846e71e7114f00cb6d13a79df0888ac8e9d7bdab
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 18:35:24 +0500

    feat: add January 1990 core contracts

--- packages/game-core/src/index.ts
+++ packages/game-core/src/index.ts
@@ -3,6 +3,26 @@ export { fingerprint, sha256Hex, stableId } from "./determinism/hash";
 export type { RandomSource } from "./determinism/random-source";
 export { Xoshiro256StarStar } from "./determinism/xoshiro256ss";
 export {
+  JANUARY_1990_CONTENT_IDS,
+  JANUARY_1990_REASON_CODES,
+  JANUARY_1990_REQUIRED_CHUNK_IDS,
+  JANUARY_1990_STABLE_IDS,
+} from "./january-1990";
+export type {
+  January1990ContentContext,
+  January1990ContentId,
+  January1990ReasonCode,
+  JanuaryAccessRoute,
+  JanuaryEventDefinition,
+  JanuaryLearningActivity,
+  JanuaryProjectDefinition,
+  JanuaryQuality,
+  JanuarySituationDefinition,
+  JanuarySkillDefinition,
+  JanuaryTechnologyContext,
+  JanuaryWorkPackage,
+} from "./january-1990";
+export {
   checkMonthRunCompatibility,
   createMonthRunCheckpoint,
   restoreMonthRunCheckpoint,
