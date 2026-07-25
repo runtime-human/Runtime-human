50ace082551a7159b913c5ebacd08dbbc3ee66a0
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 18:34:54 +0500

    feat: add January 1990 core contracts

---
+++ packages/game-core/src/january-1990/index.ts
@@ -0,0 +1,20 @@
+export {
+  JANUARY_1990_CONTENT_IDS,
+  JANUARY_1990_REQUIRED_CHUNK_IDS,
+  JANUARY_1990_STABLE_IDS,
+} from "./january-content-ids";
+export type { January1990ContentId } from "./january-content-ids";
+export type {
+  January1990ContentContext,
+  JanuaryAccessRoute,
+  JanuaryEventDefinition,
+  JanuaryLearningActivity,
+  JanuaryProjectDefinition,
+  JanuaryQuality,
+  JanuarySituationDefinition,
+  JanuarySkillDefinition,
+  JanuaryTechnologyContext,
+  JanuaryWorkPackage,
+} from "./january-content-context";
+export { JANUARY_1990_REASON_CODES } from "./january-reason-codes";
+export type { January1990ReasonCode } from "./january-reason-codes";
