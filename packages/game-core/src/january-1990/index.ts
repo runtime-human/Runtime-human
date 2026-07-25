c607640ae5da22bd483e9e68b06365e0ebbab51b
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 20:15:38 +0500

    feat: export January MonthRun contracts

--- packages/game-core/src/january-1990/index.ts
+++ packages/game-core/src/january-1990/index.ts
@@ -1,9 +1,13 @@
+export type {
+  JanuaryAccessAnswerV1,
+  JanuaryDefectAnswerV1,
+  JanuaryLearningAnswerV1,
+} from "./january-answers";
 export {
-  JANUARY_1990_CONTENT_IDS,
-  JANUARY_1990_REQUIRED_CHUNK_IDS,
-  JANUARY_1990_STABLE_IDS,
-} from "./january-content-ids";
-export type { January1990ContentId } from "./january-content-ids";
+  parseJanuaryAccessAnswer,
+  parseJanuaryDefectAnswer,
+  parseJanuaryLearningAnswer,
+} from "./january-answers";
 export type {
   January1990ContentContext,
   JanuaryAccessRoute,
@@ -16,5 +20,15 @@ export type {
   JanuaryTechnologyContext,
   JanuaryWorkPackage,
 } from "./january-content-context";
+export {
+  JANUARY_1990_CONTENT_IDS,
+  JANUARY_1990_REQUIRED_CHUNK_IDS,
+  JANUARY_1990_STABLE_IDS,
+} from "./january-content-ids";
+export type { January1990ContentId } from "./january-content-ids";
+export { JANUARY_1990_DECISION_IDS } from "./january-decisions";
+export type { January1990DecisionId } from "./january-decisions";
+export { createJanuary1990MonthPlan } from "./january-month-plan";
+export type { January1990MonthPlanV1 } from "./january-month-plan";
 export { JANUARY_1990_REASON_CODES } from "./january-reason-codes";
 export type { January1990ReasonCode } from "./january-reason-codes";
