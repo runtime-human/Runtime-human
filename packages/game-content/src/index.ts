a8bc1c2c6c64a824a2676cf8d65bfbb5c7ca5855
MrFr3di <ynagi-wm@mail.ru>
2026-09-05 17:46:50 +0500

    feat(content): export certified January scenario artifact

--- packages/game-content/src/index.ts
+++ packages/game-content/src/index.ts
@@ -4,3 +4,4 @@ export * from "./content-errors";
 export { createCompiledContentRuntime } from "./content-loader";
 export type { CompiledContentRuntime } from "./content-loader";
 export type { ContentRegistry } from "./content-registry";
+export { JANUARY_1990_SCENARIO_ARTIFACT } from "./january-1990-scenario-artifact";
